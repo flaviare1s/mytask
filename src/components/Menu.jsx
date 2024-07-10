02b739924f696fbcdce685d5791edd8805a883f8
flaviare1s <flaviarster@gmail.com>
2024-07-10 15:13:59 -0300

    Aula sobre React Bootstrap

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,3 +1,4 @@
+import { Container, Navbar, Nav } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 // Link => esse componente habilita o SPA (Single Page Application)
@@ -6,12 +7,21 @@ import { Link } from "react-router-dom"
 const Menu = () => {
   return (
     <header>
-      <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login</Link>
-        <Link to='/cadastro'>Cadastro</Link>
-        <Link to='ajuda'>Ajuda</Link>
-      </nav>  
+     <Navbar bg='dark' variant="dark" expand="sm">
+      <Container fluid>
+				<Link to='/'>
+					<img src="https://cdn.pixabay.com/photo/2017/09/29/00/30/checkmark-icon-2797531_640.png" width="32"/>
+				</Link>
+				<Navbar.Toggle />
+				<Navbar.Collapse>
+					<Nav className="ms-auto">
+						<Link className="nav-link" to='/login'>Login</Link>
+						<Link className="nav-link" to='/cadastro'>Cadastro</Link>
+						<Link className="nav-link" to='ajuda'>Ajuda</Link>
+					</Nav>
+				</Navbar.Collapse>
+      </Container>
+     </Navbar>
     </header>
   )
 }
--- src/pages/Ajuda.jsx
+++ src/pages/Ajuda.jsx
@@ -1,7 +1,33 @@
+import { Accordion, Alert, Button, Container } from "react-bootstrap"
+
 const Ajuda = () => {
   return (
-    <main>
-      <h1>Ajuda</h1>
+    <main className="mt-3">
+      <Container>
+        <h1>Ajuda</h1>
+        <hr />
+        <Accordion defaultActiveKey='0'>
+          <Accordion.Item eventKey='0'>
+            <Accordion.Header>Sobre o nosso sistema</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                MyTask é um sitema para gerenciamento de tarefas pessoais. Nessa aplicação, você pode gerenciar todos os seus afazeres.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey='1'>
+            <Accordion.Header>Sobre a empresa</Accordion.Header>
+            <Accordion.Body>
+              <p>
+                Essa empresa desenvolve soluções focadas em produtividade e bem-estar.
+              </p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+        <Button className="mt-1 me-1">Entre em contato</Button>
+        <Button className="mt-1" variant="outline-danger">Denunciar</Button>
+        <Alert className="mt-1" variant="warning">Atenção! Estamos em manutenção!</Alert>
+      </Container>
     </main>
   )
 }
