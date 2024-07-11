211a10c0e383a21d873708de286719d6085a8cb5
flaviare1s <flaviarster@gmail.com>
2024-07-11 08:32:41 -0300

    Organizei a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,62 @@
-import { Card } from 'react-bootstrap'
+import { Button, Card, Container } from 'react-bootstrap'
 
 const Home = () => {
   return (
     <main>
-      <h1>Vamos estudar alguns elementos do Bootstrap</h1>
+      <h1 className='p-3'>Bem-vindo(a) ao MyTask, o seu organizador de tarefas!</h1>
 
-      <h2>Cards</h2>
+      <Container className='d-flex gap-3'>
+        <Card border="primary" variant='dark' style={{ width: '22rem' }}>
+          <Card.Header className='text-primary fw-bold'>Organize Suas Tarefas com Facilidade</Card.Header>
+          <Card.Body>
+            <Card.Title>Maximize sua produtividade com o MyTask. Planeje, organize e gerencie suas tarefas diárias de forma simples e eficiente.</Card.Title>
+            <Card.Text>
+              <Button variant="outline-primary" className='me-3'>Comece agora</Button>
+              <Button variant="primary">Saiba mais</Button>
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-      <Card border="primary" variant='dark' style={{ width: '18rem' }}>
-        <Card.Header>Esse é o header do card</Card.Header>
-        <Card.Body>
-          <Card.Title>Esse é o title do card</Card.Title>
-          <Card.Text>
-            Esse card tem a borda primary.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        <Card border="danger" variant='dark' style={{ width: '22rem' }}>
+          <Card.Header className='text-danger fw-bold'>Funcionalidades Poderosas</Card.Header>
+          <Card.Body>
+            <Card.Title>Descubra as ferramentas que farão você alcançar seus objetivos.</Card.Title>
+            <Card.Text>
+              <ul className='text-danger'>
+                <li>Listas Personalizadas</li>
+                <li>Lembretes</li>
+                <li>Calendário</li>
+                <li>Notificações</li>
+              </ul>
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-      <Card className='mt-3' border="danger" variant='dark' style={{ width: '18rem' }}>
-        <Card.Header>Esse é o header do card</Card.Header>
-        <Card.Body>
-          <Card.Title>Esse é o title do card</Card.Title>
-          <Card.Text>
-            Esse card tem a borda danger.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+        <Card border="warning" variant='dark' style={{ width: '22rem' }}>
+          <Card.Header className='text-warning fw-bold'>Por Que Escolher o MyTask?</Card.Header>
+          <Card.Body>
+            <Card.Title>Veja como o MyTask pode transformar sua rotina.</Card.Title>
+            <Card.Text>
+              <ul className='text-warning'>
+                <li>Organize suas tarefas com facilidade</li>
+                <li>Acessível em qualquer dispositivo</li>
+                <li>Suporte ao cliente</li>
+              </ul>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card border="success" variant='dark' style={{ width: '22rem' }}>
+          <Card.Header className='text-success fw-bold'>Pronto para Começar?</Card.Header>
+          <Card.Body>
+            <Card.Title>Junte-se a milhares de usuários satisfeitos que já estão aproveitando ao máximo suas rotinas com o MyTask.</Card.Title>
+            <Card.Text>
+              <Button variant="success" className='me-3'>Crie uma conta</Button>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+      </Container>
     </main>
   )
 }
