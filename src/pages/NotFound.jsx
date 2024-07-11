d9b8b433b30c4c4b1e8a16a2de0c52a5fd0daf00
flaviare1s <flaviarster@gmail.com>
2024-07-11 08:13:44 -0300

    Alterei a página not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,14 +1,20 @@
-import { Button } from "react-bootstrap"
+import { Button, Container } from "react-bootstrap"
 import { Link } from "react-router-dom"
+import error from "../assets/error.jpg"
 
 const NotFound = () => {
   return (
     <main>
-      <h1>Página não encontrada. 404.</h1>
+      <h1 className="text-center text-danger">Página não encontrada. 404.</h1>
       <hr />
-      <Link to='/'>
-        <Button variant="primary">Voltar para Home</Button>
-      </Link>
+      <Container fluid>
+        <Container>
+          <img  className="img-fluid" src={error} alt="" />
+        </Container>
+        <Link to='/'>
+          <Button className="mt-3" variant="primary">Voltar para Home</Button>
+        </Link>
+      </Container>
     </main>
   )
 }
