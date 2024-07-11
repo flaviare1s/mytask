2d10b2d4d471ebdd206919b5e50d0036bf2d5208
flaviare1s <flaviarster@gmail.com>
2024-07-11 10:19:40 -0300

    Comecei a fazer o form de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
+import { Button } from "react-bootstrap"
+
 const Login = () => {
   return (
     <main>
-      <h1>Login</h1>
+      <form className="form-section">
+        <div>
+          <label htmlFor="email">E-mail</label>
+          <input type="email" id="email" className="form-control" />
+        </div>
+        <div>
+          <label htmlFor="senha">Senha</label>
+          <input type="password" id="senha" className="form-control" />
+        </div>
+        <Button variant="dark" className="mt-1 w-100" type="submit">Entrar</Button>
+        <Button variant="danger" className="mt-1 w-100" type="button">Entrar com o Google</Button>
+      </form>
     </main>
   )
 }
