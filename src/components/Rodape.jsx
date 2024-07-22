0eb93a055c466d4a8d432b11912464548046e424
flaviare1s <flaviarster@gmail.com>
2024-07-22 19:34:25 -0300

    Coloquei nome e link no Footer

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -5,8 +5,8 @@ const Rodape = () => {
   return (
     <footer>
       <Container fluid className="text-center bg-dark py-3">
-        <small className="text-secondary">&copy; 2024. MyTask. Todos os direitos reservados. </small>
-        <Link className="text-secondary text-decoration-none small" to='/politicaprivacidade'>Veja nossa política de privacidade clicando aqui!</Link>
+        <small className="text-secondary">&copy; 2024. Desenvolvido por <a href="https://github.com/flaviare1s" className="text-secondary text-decoration-none fw-bold">Flávia Reis</a>.</small>
+        <Link className="text-secondary text-decoration-none small" to='/politicaprivacidade'> Veja nossa política de privacidade clicando aqui!</Link>
       </Container>
     </footer>
   )
